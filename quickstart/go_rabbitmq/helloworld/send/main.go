9fe44145f2568a96066b422bea728eb3dd1c05d7
wangzhe0912 <wangzhe0912@tju.edu.cn>
2022-01-07 10:48:16 +0800

    add logger

--- quickstart/go_rabbitmq/helloworld/send/main.go
+++ quickstart/go_rabbitmq/helloworld/send/main.go
@@ -4,10 +4,21 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 	logger "github.com/sirupsen/logrus"
 	"github.com/wagslane/go-rabbitmq"
+	"log"
 )
 
+type customLogger struct{}
+
+// Printf is the only method needed in the Logger interface to function properly.
+func (c *customLogger) Printf(fmt string, args ...interface{}) {
+	log.Printf("mylogger: " + fmt, args...)
+}
+
 func main() {
-	publisher, err := rabbitmq.NewPublisher("amqp://user:password@localhost:5672/", amqp.Config{})
+	mylogger := &customLogger{}
+	publisher, err := rabbitmq.NewPublisher("amqp://user:password@localhost:5672/", amqp.Config{},
+		rabbitmq.WithPublisherOptionsLogger(mylogger))
+
 	if err != nil {
 		logger.Fatal(err)
 	}
